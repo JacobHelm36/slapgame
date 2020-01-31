d01b8cfca5496d7cbd5007e157a48b9b9ad94242
jhelm3 <jakehelm36@gmail.com>
2020-01-31 09:58:13 -0700

    removed a few functions

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 let cookieObj = {
   health: 100,
   hitCount: 0,
+  status: [
+    {
+      level: "strong",
+      img: ""
+    },
+    {
+      level: "Taking hits",
+      img: "",
+    },
+    {
+      level: "Falling apart",
+      img: ""
+    },
+    {
+      level: "Thats a tough cookie",
+      img: ""
+    }
+  ]
 }
 let clicker = { attacks: {
   slap: 1,
@@ -14,44 +32,69 @@ let items = {
   butter: {name: "Butter", modifier: 3, description:"butter good"},
 }
 
-let healthElem = document.getElementById("health").innerHTML
+let healthElem = document.getElementById("health").textContent
 let hitcountElem = document.getElementById("hit-count").textContent
-
+let cookieLevelElem = document.getElementById("cookie-level")
+let cookieImgElem = document.getElementById("cookie-image")
 
 let modifiers = 0
 //loops thru the clicker items and adds them
 function addMods(){
-  modifiers = 0;
   for(let i = 0; i < clicker.items.length; i++) {
     modifiers += clicker.items[i].modifier;
     console.log(modifiers)
     return modifiers;
   }
 }
+//Getting items damage to stack on top of eachother?
+//First item chose is the only one that will add?
 
-
-//pushes items into the clicker items array
+let itemCount = 0
+//pushes items into the clicker items array, all my items line (94)
 function knifers() {
-  document.getElementById("knife").textContent
+  if (itemCount == 1){
+    document.getElementById("item-limit").textContent = "You can only take this item once"
+    itemCount--
+    clicker.items.pop
+  } else {
+  itemCount++
+  document.getElementById("knife")
   clicker.items.push(items.knife)
+  console.log(modifiers)
   addMods()
+  }
 }
 
+
 function battleScream() {
-  document.getElementById("battle-cry").textContent
+  if (itemCount == 1){
+    document.getElementById("item-limit").textContent = "You can only take one item"
+    itemCount--
+    clicker.items.pop
+  } else {
+  itemCount++
+  document.getElementById("battle-cry")
   clicker.items.push(items.battleScream)
+  console.log(modifiers)
   addMods()
+  }
 }
 
 function butters() {
+  if (itemCount == 1){
+    document.getElementById("item-limit").textContent = "You can only take this item once"
+    itemCount--
+    clicker.items.pop()
+  } else {
   document.getElementById("but").textContent
   clicker.items.push(items.butter)
+  console.log(modifiers)
   addMods()
+  }
 }
 
-//Which one is better?
-//Why doesnt this work?
-//Add this damage to an object?
+
+//attacks end line (118)
 function attacks(slappy){
   if (slappy == "slap") {
     cookieObj.health -= modifiers+1;
@@ -75,23 +118,22 @@ function attacks(slappy){
 }
 attacks()
 
-function knockedOut(){
-  document.getElementById("").textContent
-  //get a KO'd picture here
-}
 
-//location.reload() restarts the page, set it after an alert.
+function knockedOut(){
+  //get a KO'd picture here
+  document.getElementById("weariness").textContent = "Defeated"
+  document.getElementById("cookie-level").textContent = "A picture of a defeated cookie!"
+  alert(location.reload())
+}
 
 
 function update() {
   document.getElementById("health").textContent = cookieObj.health.toString()
   document.getElementById("hit-count").textContent = cookieObj.hitCount.toString()
-  if (cookieObj.health<=0) {
-    cookieObj.health = 0;
-  }
 }
-update()
 
+
+//for the status images of the cookie
 function wearyCookie() {
   let weary = document.getElementById("weariness").innerHTML
   if (cookieObj.health <75) {
@@ -107,3 +149,7 @@ function wearyCookie() {
     weary = "I'm crumbled, but I'll be back!!"
   }
 }
+
+
+//location.reload() restarts the page, set it after an alert.
+//google search transparent background images
